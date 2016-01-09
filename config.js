588bca1107d692500b0f2e686b60c704e58193e6
farazi <farazi@Shumanas-MacBook-Pro.local>
2016-01-09 16:45:22 +0600

    make it better

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
     'secret': 'ilovescotchyscotch',
-    'database': 'mongodb://herokuuser:herokuuser.mongolab.com:37415/conferencemanagement'
+    'database': 'mongodb://herokuuser:herokuuser@ds037415.mongolab.com:37415/conferencemanagement'
 
 };
